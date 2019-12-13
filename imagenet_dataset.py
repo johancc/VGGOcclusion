ea1f5654a96c217fc49f936eadf6027aa4d81932
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2019-12-12 20:30:18 -0500

    Added imagenet data

---
+++ imagenet_dataset.py
@@ -0,0 +1,42 @@
+"""
+Loads the dataset from image net.
+"""
+from utils import preprocess_image, create_dataset_from_main_directory
+from torch.utils import data
+
+
+
+class ImageNetData(data.Dataset):
+    """
+    Dataset of various classes from ImageNet
+    """
+    def __init__(self, data_root: str = "image_net_data/", limit=-1):
+        self.samples = []
+        self.data_root = data_root
+        self._init_dataset(limit)
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, index):
+        img_path, label = self.samples[index]
+        return preprocess_image(img_path), label
+
+    def _init_dataset(self, limit: int):
+        """
+        Populates the samples list with a tuple (path, label)
+        The images are not loaded since it would be memory
+        inefficient.
+        """
+
+        dataset = create_dataset_from_main_directory(self.data_root)
+        for image_path, label in dataset.items():
+            if label is not None:
+                self.samples.append([image_path, label])
+                if len(self.samples) >= limit != -1:
+                    break
+
+
+if __name__ == '__main__':
+    data = ImageNetData()
+    print(len(data))
---
+++ utils.py
@@ -0,0 +1,82 @@
+"""
+Utils needed to handle ImageNet data.
+"""
+import requests
+import os
+from PIL import Image
+from torch.autograd import Variable
+from torchvision.transforms import transforms
+
+LABELS_URL = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
+response = requests.get(LABELS_URL)  # Make an HTTP GET request and store the response.
+labels = {int(key): value for key, value in response.json().items()}
+
+
+def class_name_to_id(class_name: str) -> int:
+    """
+    :param class_name the name of the class
+    :returns the index for the class name in the labels dict, None if not found.
+    """
+    for i, names in labels.items():
+        # the value of the values contains multiple possible class names
+        if "," in names:
+            names = names.split(",")
+        else:
+            names = [names]
+        for name in names:
+            if name.strip() == class_name.strip():
+                return i
+
+
+def filepath_to_label(filepath: str)->int:
+    """
+    Finds the id of the image based on the filepath.
+    Assumes that the name of the folder the image is contained in is the class name.
+    i.e some/path/bird/123.jpg for class bird
+    """
+    root_path, folder_name = os.path.split(os.path.dirname(filepath))
+    return class_name_to_id(folder_name)
+
+
+def create_dataset_from_main_directory(directory: str) -> dict:
+    """
+    Makes a mapping between the filename and the class label.
+    Expects the directory to contain sub folders where their name
+    are the class name.
+    """
+    dataset = {}
+    for (dirpath, dirnames, filenames) in os.walk(directory):
+        if dirpath == directory:
+            continue
+        for filename in filenames:
+            path = os.path.join(dirpath, filename)
+            dataset[path] = filepath_to_label(path)
+    return dataset
+
+
+img_size = 224
+transform_pipeline = transforms.Compose(
+    [transforms.Resize((img_size, img_size)),
+     transforms.ToTensor(),
+     transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225])]
+)
+
+
+def preprocess_image(img_path: str):
+    """
+    The network needs the image to be scaled to 224 by 224.
+    The image is normalized based on how the pre-trained model was trained.
+    """
+    img = Image.open(img_path)
+    # Now we need to preprocess the image
+
+    img = transform_pipeline(img)
+    # Tensor Dims = (num_input_images,
+    #               num_color_channels,
+    #               height,
+    #               width)
+    img = img.unsqueeze(0)  # new first axis
+    img = Variable(img)  # input should be a Variable type.
+
+    return img
